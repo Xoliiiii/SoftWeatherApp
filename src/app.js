8dbe2c358f530d316761eda8108c4bd542240cc8
Xolisa <109094089+Xoliiiii@users.noreply.github.com>
2023-07-27 21:27:18 +0200

    update time

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
+function formatDate(timestamp){
+    //Calculate the date
+let date = new Date (timestamp);
+let hours = date.getHours();
+if (houra<10){
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes<10){
+    minutes = `0${minutes}`;
+}
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"]
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response){
     console.log(response.data)
    let temperature = document.querySelector("#temperature");
    let city = document.querySelector("#city");
-   let description = document.querySelector("#description")
+   let description = document.querySelector("#description");
+   let date = document.querySelector("#date");
    temperature.innerHTML= Math.round(response.data.main.temp);
    city.innerHTML = response.data.name
    description.innerHTML=response.data.weather[0].description;
+   date.innerHTML = formatDate(response.data.dt* 1000);
 }
-
 let apiKey = "7d5e99a2cfae1163cb682076044b282d";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
