fe128ba5f36aea805ae4a1b5591721bdf1dfd307
Xolisa <109094089+Xoliiiii@users.noreply.github.com>
2023-08-02 17:11:49 +0200

    Search Engine and API integration
    I created the search engine to run user input with the help of an API to get my results

--- src/app.js
+++ src/app.js
@@ -14,22 +14,81 @@ let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+    let date = new Date(timestamp*1000);
+    let day = date.getDay();
+    let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+    return days[day];
+}
+
+function displayForecast(response){
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast");
+    
+    let forecastHTML = `<div class="row">`;
+    forecast.foreach(function (forecast,index)
+    {
+        if(index<6) {
+            forecastHTML = forecastHTML +
+            `
+            <div class="col-2">
+            <div class='weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png"
+            alt="" width="42"/>
+            <div class="weather-forecasttemperature">
+            <span class="weather-forecast-temperature-max">${Math.round(forecastDay.data.temperature)}°</span>
+            </div>
+            </div>
+            `;
+
+        }
+    });
+
+    forecastHTML = forecastHTML + `</div>`
+    forecastElement.innerHTML = forecastHTML;
+
+}
+
+function getForecast (coordinates){
+let apiKey = "de0bco366bc60bae2409dfb13a7t7749";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${coordinates.lon}&lat=${coordinates.lat}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
+}
 
 function displayTemperature(response){
-    console.log(response.data);
    let temperature = document.querySelector("#temperature");
    let city = document.querySelector("#city");
    let description = document.querySelector("#description");
    let date = document.querySelector("#date");
    let icon = document.querySelector("#icon");
+
    temperature.innerHTML= Math.round(response.data.temperature.current);
-   city.innerHTML = response.data.city
+   city.innerHTML = response.data.city;
    description.innerHTML=response.data.condition.description;
    date.innerHTML = formatDate(response.data.time* 1000);
-   icon.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`)
+   icon.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`); 
+   getForecast(response.data.coordinates);
+   
 }
-let apiKey = "de0bco366bc60bae2409dfb13a7t7749";
-let city = "Port Elizabeth"
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city){
+    let apiKey = "de0bco366bc60bae2409dfb13a7t7749";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    let cityName = document.querySelector("#city-name");
+    let cityInput= document.querySelector("#city");
+    cityInput.innerHTML = `${cityName.value}`;
+    search(cityName.value);
+}
+
+let form = document.querySelector ("#search-form");
+form.addEventListener("submit",handleSubmit);
+
+search ("Port Elizabeth");
